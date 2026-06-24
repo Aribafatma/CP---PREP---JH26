c12492d3a38f94e9cffef052c08f7ee3f1a5b7a4
Aribafatma <aribaf70@gmail.com>
2026-06-24 17:12:47 +0530

    Initial commit

---
+++ 1.Arrays/sum-of-array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i = 0; i<= n-1; i++){
+        cin >> a[i];
+    }
+    int sum = 0;
+    for (int i = 0; i<= n-1; i++){
+        sum += a[i];
+    }
+    cout << sum << endl;
+}
